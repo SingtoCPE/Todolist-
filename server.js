7ab163bbd5b28fc305de1a26fb723f84b71bc758
thitipat <thitipat.cpe18@hotmail.com>
2019-07-25 16:04:41 +0700

    use postman services api

--- server.js
+++ server.js
@@ -1,41 +1,67 @@
-const express = require('express')                                // เรียกใช้ Express
+const express = require('express')                               
 const bodyParser = require('body-parser');
-const mysql = require('mysql')                                    // เรียกใช้ mysql
-const db = mysql.createConnection({                               // config ค่าการเชื่อมต่อฐานข้อมูล
+const mysql = require('mysql')                                    
+const db = mysql.createConnection({                               
   host     : 'localhost', 
   user     : 'root',
   password : 'singto11442525',
   database : 'myDB'
 })
 
-db.connect()                                                      // เชื่อมต่อฐานข้อมูล
-const app = express()                                             // สร้าง Object เก็บไว้ในตัวแปร app เพื่อนำไปใช้งาน
-// Select Data
-    app.use(bodyParser.json() );                                  // to support JSON-encoded bodies
-    app.use(bodyParser.urlencoded({                               // to support URL-encoded bodies
-      extended: true
-    })); 
-
-    app.post("/adduser", function(req, res) {
-    // get data from forms and add to the table called user..
-      var id = req.body.id;
-      var name = req.body.name;
-      console.log(id, name);
+  db.connect()                                                      
+  const app = express()                                            
+  app.use(bodyParser.json() );                                  
+  app.use(bodyParser.urlencoded({extended: true})); 
+//-------------------------------------------------------------------------------
+  app.post("/adduser", function(req, res) {
+    console.log(req.body.length);
+    
+    var i;
+    for (i = 0; i < req.body.length; i++) { 
+      var item = req.body[i];
+      var id = item.id;
+      var name = item.name;
+    
+      //console.log(id, name);
       db.query("INSERT INTO myDB.myTB (id, name) VALUES ('" + id + "', '" + name + "')", function (err, result) {
-        if (err) throw err;
-      console.log("1 record inserted");
-      });
-      res.redirect("/");
-    })
-
-app.get('/',(req,res)=> {                                     // Router เวลาเรียกใช้งาน
-  let sql = 'SELECT * FROM myDB.myTB;'                              // คำสั่ง sql
-  let query = db.query(sql,(err,results) => {                       // สั่ง Query คำสั่ง sql
-    if(err) throw err                                                 // ดัก error
-  console.log(results)                                              // แสดงผล บน Console 
-  res.json(results)                                                 // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+      if (err) throw err;
+      //console.log("1 record inserted");
+    });
+    
+    }
+    res.redirect("/");
   })
-})
-app.listen('3000',() => {     //
-console.log('start port 3000')  
-})
+//-------------------------------------------------------------------------------
+
+  app.get('/',(req,res)=> {                        //get all             
+    let sql = 'SELECT * FROM myDB.myTB;'                              
+    let query = db.query(sql,(err,results) => {                       
+      if(err) throw err                                                
+      console.log(results)                                             
+      res.json(results)                                                
+    })
+  })
+//-------------------------------------------------------------------------------
+  app.get('/id/s2',(req,res)=> {                        //get some information           
+    let sql = "SELECT * FROM myDB.myTB WHERE id = 's2'"                              
+    let query = db.query(sql,(err,results) => {                       
+      if(err) throw err                                                
+      console.log(results)                                             
+      res.json(results)                                                
+    })
+  })
+//-------------------------------------------------------------------------------  
+  app.delete('/deluser', function (req, res) {
+    
+    let sql = `DELETE FROM myDB.myTB WHERE orders between 16 and 30`;
+    let query = db.query(sql,(err,results) => {                       
+      if(err) throw err                                                
+      console.log("1 record deleted");
+    });
+    res.redirect("/");                                            
+  })
+//-------------------------------------------------------------------------------
+
+  app.listen('3000',() => {     //
+    console.log('start port 3000')  
+  })
